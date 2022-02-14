a4d088ef757a3624094db51e10cf07f897395e23
Luís Passos <luis.passos013@gmail.com>
2022-02-14 14:57:47 -0300

    added images in public folder and Header component

---
+++ src/components/index.tsx
@@ -0,0 +1,16 @@
+import Image from 'next/image'
+import styles from './styles.module.scss'
+
+export function Header() {
+  return (
+    <header className={styles.headerContainer}>
+      <div className={styles.headerContent}>
+        <Image src="/images/logo.svg" alt="ig.news" />
+        <nav>
+          <a>Home</a>
+          <a>Posts</a>
+        </nav>
+      </div>
+    </header>
+  )
+}
