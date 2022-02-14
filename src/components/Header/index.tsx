0b6758da0dd3f7a0f99717c8ccb20cf56ef8a20b
luis <luis.passos013@gmail.com>
2022-02-14 17:09:59 -0300

    full home page style

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,15 +1,17 @@
-import Image from 'next/image'
+import { SignInButton } from '../SignInButton'
 import styles from './styles.module.scss'
 
 export function Header() {
   return (
     <header className={styles.headerContainer}>
       <div className={styles.headerContent}>
-        <Image src="/images/logo.svg" alt="ig.news" />
+        <img src="/images/logo.svg" alt="ig.news" />
         <nav>
-          <a>Home</a>
+          <a className={styles.active}>Home</a>
           <a>Posts</a>
         </nav>
+
+        <SignInButton />
       </div>
     </header>
   )
