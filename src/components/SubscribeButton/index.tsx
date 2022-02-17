e5e624ca68658f5b0d82d5561c87a27a80a298e3
luis <luis.passos013@gmail.com>
2022-02-16 22:00:11 -0300

    added post page and its preview page

--- src/components/SubscribeButton/index.tsx
+++ src/components/SubscribeButton/index.tsx
@@ -1,4 +1,5 @@
 import { signIn, useSession } from 'next-auth/react'
+import { useRouter } from 'next/router'
 import { api } from '../../services/api'
 import { getStripeJs } from '../../services/stripe-js'
 import styles from './styles.module.scss'
@@ -10,10 +11,16 @@ interface SubscribeButtonProps {
 export function SubscribeButton({ priceId }: SubscribeButtonProps) {
 
   const session = useSession()
+  const router = useRouter()
 
   async function handleSubscribe() {
     if(!session.data) {
       signIn('github')
+      return
+    }
+
+    if(session.data.activeSubscription) {
+      router.push('/posts')
       return
     }
 
