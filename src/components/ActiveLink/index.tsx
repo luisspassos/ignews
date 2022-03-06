38612a1196a42d5fdd1fafa7d4df4f3d8d7ed7c2
luis <luis.passos013@gmail.com>
2022-03-06 17:43:44 -0300

    remove console.log about paths in ActiveLink

--- src/components/ActiveLink/index.tsx
+++ src/components/ActiveLink/index.tsx
@@ -13,8 +13,6 @@ export function ActiveLink({ children, activeClassName, ...rest }: ActiveLinkPro
 
   const className = asPath === rest.href ? activeClassName : ''
 
-  console.log(asPath, rest.href)
-
   return (
     <Link {...rest}>
       {cloneElement(children, {
